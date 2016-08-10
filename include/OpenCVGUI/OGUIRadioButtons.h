125920c28ef2b4bf69936f656c3d12d4a693654e
David Millan (Damiles) <david.millan@damiles.com>
2016-08-10 11:46:45 +0200

    Radio button widget

---
+++ include/OpenCVGUI/OGUIRadioButtons.h
@@ -0,0 +1,29 @@
+//
+// Created by damiles on 10/08/16.
+//
+
+#ifndef OPENCVGUI_OGUIRADIOBUTTONS_H
+#define OPENCVGUI_OGUIRADIOBUTTONS_H
+
+#include <vector>
+#include <string>
+#include <functional>
+#include "OGUIWidget.h"
+
+namespace OpenCVGUI {
+    class OGUIRadioButtons : public OGUIWidget {
+    public:
+        virtual void draw(int x, int y, int width);
+        OGUIRadioButtons(const char *title, std::vector<std::string> options);
+        void setCallBack(std::function<void(int)> func);
+
+    private:
+        const char *title_;
+        std::vector <std::string> options_;
+        int selected_;
+
+        std::function<void(int)> on_change_callback;
+    };
+}
+
+#endif //OPENCVGUI_OGUIRADIOBUTTONS_H
