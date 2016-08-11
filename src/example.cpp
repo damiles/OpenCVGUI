522ad9a67d041426d9fef092a007c6bfa08ae536
David Millan (Damiles) <david.millan@damiles.com>
2016-08-11 09:36:18 +0200

    Example with linux screen capture sample

--- src/example.cpp
+++ src/example.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <opencv2/core/core.hpp>
@@ -10,9 +9,18 @@
 #include "OGUIWidget.h"
 #include "OGUIButton.h"
 #include "OGUISlider.h"
+#include "OGUITextInput.h"
+#include "OGUIRadioButtons.h"
+
+#ifdef X11
+#include <X11/Xlib.h>
+#endif
+
 
 using namespace OpenCVGUI;
 using namespace cv;
+
+VideoCapture camera;
 
 void hello_btn_click(){
     cout << "HELLO clicked" << endl;
@@ -25,32 +33,68 @@ void exit_btn_click(){
     quit=1;
 }
 
+void on_change_camera(int option){
+    camera.open(option);
+}
+
 int main( int argc, const char* argv[] )
 {
     srand (time(NULL));
-
 
 	OpenCVGUI::init();
 	OGUIWindow window(1024, 768, "Wellcome to OpenCVGUI 1");
 
 	Mat frame;
-	VideoCapture camera(0);
-	if(!camera.isOpened()){
-        cout << "Error, no camera found" << endl;
-		return -1;
-    }
+
 
     OGUIFormArea formArea(&window);
     OGUILayout layout1(&window, 1);
     window.addArea(&formArea);
     window.addArea(&layout1);
 
-    OGUIButton button("Hello");
+//    OGUISlider slider("Threshold", 0, 255, 125);
+//    formArea.addWidget(&slider);
+
+    OGUITextInput textInputId("Id");
+    formArea.addWidget(&textInputId);
+
+    OGUITextInput textInputName("Name");
+    formArea.addWidget(&textInputName);
+
+    OGUITextInput textInputEmail("Email");
+    formArea.addWidget(&textInputEmail);
+
+    vector<string> cameras;
+    int num_cameras=0;
+    for(num_cameras=0; num_cameras<10; num_cameras++){
+        if(!camera.open(num_cameras)) {
+            break;
+        }else{
+            stringstream ss;
+            ss << "Camera " << num_cameras;
+            cameras.push_back(ss.str());
+        }
+    }
+
+    if(num_cameras==0){
+        cout << "Error, no cameras found" << endl;
+		return -1;
+    }
+
+    // Initialize with first camera
+    camera.open(0);
+
+    OGUIRadioButtons camera_list("Cameras", cameras);
+    camera_list.setCallBack(on_change_camera);
+    formArea.addWidget(&camera_list);
+
+    OGUIButton button("Start Record");
     button.setCallBack(hello_btn_click);
     formArea.addWidget(&button);
 
-    OGUISlider slider("Threshold", 0, 255, 125);
-    formArea.addWidget(&slider);
+    OGUIButton buttonStop("Stop Record");
+    buttonStop.setCallBack(hello_btn_click);
+    formArea.addWidget(&buttonStop);
 
     OGUIButton button1("Exit");
     button1.setCallBack(exit_btn_click);
@@ -67,15 +111,51 @@ int main( int argc, const char* argv[] )
     layout1.addArea(&imageArea2);
     
     camera >> frame;
-    //frame= imread("../resources/icon1.png");
+//    frame= imread("../resources/icon1.png");
     imageArea1.setImage(frame);
 
+#ifdef X11
+    // make screen shot
+    Display *display = XOpenDisplay(NULL);
+    Window root = DefaultRootWindow(display);
+
+    XWindowAttributes gwa;
+
+    XGetWindowAttributes(display, root, &gwa);
+    int width = gwa.width;
+    int height = gwa.height;
+
+
+    XImage *image = XGetImage(display,root, 0,0 , width,height,AllPlanes, ZPixmap);
+    Mat img= Mat( image->height, image->width, CV_8UC4, image->data);
+    resize(img, img, Size(0,0), 0.25, 0.25);
+    cvtColor(img, img, CV_BGRA2BGR);
+
+//    imwrite("out.jpg", img);
+//    cvNamedWindow("test", WINDOW_NORMAL);
+//    imshow("test",img);
+//    cvWaitKey(0);
+
+#else
     Mat img= imread("../resources/image1.jpg");
+#endif
     imageArea2.setImage(img);
 
+
+    int count=0;
     while(true){
-        //camera >> frame;
-        //imageArea1.setImage(frame);
+        camera >> frame;
+        imageArea1.setImage(frame);
+
+//        if(count%15==0) {
+//            XImage *image = XGetImage(display, root, 0, 0, width, height, AllPlanes, ZPixmap);
+//            Mat img = Mat(image->height, image->width, CV_8UC4, image->data);
+//            resize(img, img, Size(0,0), 0.25, 0.25);
+//            cvtColor(img, img, CV_BGRA2BGR);
+//            imageArea2.setImage(img);
+//        }
+//        count++;
+
         window.update();
         //cvWaitKey(10);
         if(quit==1)
